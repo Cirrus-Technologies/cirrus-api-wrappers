6b569f3f997669396560b64c530037be6b57bb99
caelen-cater <64516705+caelen-cater@users.noreply.github.com>
2023-12-07 12:51:27 -0600

    Create index.php

---
+++ 8-ball/index.php
@@ -0,0 +1,27 @@
+<?php
+$url = 'https://api.cirrus.center/api/v1/random/8ball/';
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($ch);
+
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+if ($httpCode == 200) {
+    $decodedResult = json_decode($result, true);
+
+    if ($decodedResult !== null) {
+        header('Content-Type: application/json');
+        echo json_encode($decodedResult);
+    } else {
+        http_response_code(500);
+        echo 'Error: Unable to decode JSON response.';
+    }
+} else {
+    http_response_code($httpCode);
+    echo 'Error: Check API docs. HTTP Status Code: ' . $httpCode;
+}
+curl_close($ch);
+?>
