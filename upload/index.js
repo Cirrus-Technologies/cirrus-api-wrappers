1222ed5c85c9cc885227fcb3675027fb47a27576
caelen-cater <64516705+caelen-cater@users.noreply.github.com>
2023-12-15 15:16:12 +0000

    Finished JS wrappers

---
+++ upload/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = 3000;
+
+app.get('/upload/:link', async (req, res) => {
+    const link = req.params.link;
+    const url = `https://api.cirrus.center/api/v1/tools/upload/?file=${link}`;
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(`HTTP Code: ${response.status}`);
+            }
+        })
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.status(500).send('Error: ' + error.message);
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
